7e9d5622c655d8f5adbd46bbd02c3f5cdc7760ea
Ruan Koen <sitesafetyservices121@gmail.com>
2025-10-03 06:22:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "rak-sms-ai-original-8604-15199",
+  "appId": "1:155016620289:web:3747314d0f5bdc2bae9300",
+  "apiKey": "AIzaSyDWUALoi9wbHXpk5THJ5xEwUTKEE5RtYKE",
+  "authDomain": "rak-sms-ai-original-8604-15199.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "155016620289"
+};
