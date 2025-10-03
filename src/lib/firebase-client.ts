b31f04961486800b76d76fbdf6093b9d962262b4
Ruan Koen <sitesafetyservices121@gmail.com>
2025-10-03 06:17:12 +0000

    File changes

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -11,13 +11,12 @@ import { getFirestore, Firestore, setLogLevel } from "firebase/firestore";
 // ==============================
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  projectId: "rak-sms-ai-original-8604-15199",
+  appId: "1:155016620289:web:3747314d0f5bdc2bae9300",
+  apiKey: "AIzaSyDWUALoi9wbHXpk5THJ5xEwUTKEE5RtYKE",
+  authDomain: "rak-sms-ai-original-8604-15199.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "155016620289",
 };
 
 // ==============================
